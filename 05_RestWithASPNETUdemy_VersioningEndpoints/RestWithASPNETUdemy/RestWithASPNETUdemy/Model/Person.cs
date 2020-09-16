e9ead702fe2f3d1e163e847466a3cb2b456904c5
leandrocgsi <leandrocgsi@gmail.com>
2020-09-16 16:43:52 -0300

    Versioning Endpoints

---
+++ 05_RestWithASPNETUdemy_VersioningEndpoints/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("person")]
+    public class Person
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+        [Column("gender")]
+        public string Gender { get; set; }
+    }
+}
---
+++ 05_RestWithASPNETUdemy_VersioningEndpoints/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+        
+        // Method responsible for returning all people,
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Method responsible for returning one person by ID
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        // Method responsible to crete one new person
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        // Method responsible for updating one person
+        public Person Update(Person person)
+        {
+            // We check if the person exists in the database
+            // If it doesn't exist we return an empty person instance
+            if (!Exists(person.Id)) return new Person();
+
+            // Get the current status of the record in the database
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    // set changes and save
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
