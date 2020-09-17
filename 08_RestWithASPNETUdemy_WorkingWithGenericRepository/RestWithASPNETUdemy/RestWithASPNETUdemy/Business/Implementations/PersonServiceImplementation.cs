4594103ccad33e4335af32cb903313f9e26bf6c9
leandrocgsi <leandrocgsi@gmail.com>
2020-09-17 01:27:19 -0300

    Working With Generic Repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        // Method responsible for returning all people,
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Method responsible for returning one person by ID
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        // Method responsible to crete one new person
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // Method responsible for updating one person
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Context/MySQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+        
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
