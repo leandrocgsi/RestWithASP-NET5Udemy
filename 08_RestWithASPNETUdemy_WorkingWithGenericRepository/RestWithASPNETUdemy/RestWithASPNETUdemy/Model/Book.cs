69f939c0148889a13a88531da58ed16a7a0dbdbb
leandrocgsi <leandrocgsi@gmail.com>
2020-09-17 17:15:59 -0300

    Working With Generic Repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Book
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+    }
+
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/BookRepositoryImplementation.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Repository.Implementations
+{
+    public class BookRepositoryImplementation : IBookRepository
+    {
+
+        private MySQLContext _context;
+
+        public BookRepositoryImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+        
+        // Method responsible for returning all people,
+        public List<Book> FindAll()
+        {
+            return _context.Books.ToList();
+        }
+
+        // Method responsible for returning one book by ID
+        public Book FindByID(long id)
+        {
+            return _context.Books.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        // Method responsible to crete one new book
+        public Book Create(Book book)
+        {
+            try
+            {
+                _context.Add(book);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return book;
+        }
+
+        // Method responsible for updating one book
+        public Book Update(Book book)
+        {
+            // We check if the book exists in the database
+            // If it doesn't exist we return an empty book instance
+            if (!Exists(book.Id)) return null;
+
+            // Get the current status of the record in the database
+            var result = _context.Books.SingleOrDefault(p => p.Id.Equals(book.Id));
+            if (result != null)
+            {
+                try
+                {
+                    // set changes and save
+                    _context.Entry(result).CurrentValues.SetValues(book);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return book;
+        }
+
+        // Method responsible for deleting a book from an ID
+        public void Delete(long id)
+        {
+            var result = _context.Books.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Books.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public bool Exists(long id)
+        {
+            return _context.Books.Any(p => p.Id.Equals(id));
+        }
+    }
+}
