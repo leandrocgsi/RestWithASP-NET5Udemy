b1c05a9d110c8937ec457c49377ea3a26959e531
leandrocgsi <leandrocgsi@gmail.com>
2020-09-18 01:41:09 -0300

    Custom Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,15 @@
+﻿namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
