55731ca133e45e26603fce0c5cacc29a8b96af1d
leandrocgsi <leandrocgsi@gmail.com>
2020-09-18 20:00:44 -0300

    Adding Authentication Support

---
+++ 14_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("person")]
+    public class Person : BaseEntity
+    {
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+        [Column("gender")]
+        public string Gender { get; set; }
+    }
+}
---
+++ 14_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
