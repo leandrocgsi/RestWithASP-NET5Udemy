6a43fa9dcc529e09ad9195227614d19ea2028cfe
leandrocgsi <leandrocgsi@gmail.com>
2020-09-19 19:51:36 -0300

    Adding Authentication Support

---
+++ 15_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/Abstract/ISupportsHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Hypermedia.Abstract
+{
+    public interface ISupportsHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
