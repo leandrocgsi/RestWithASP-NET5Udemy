244c874be2e5f2d5e31b589b98e3ed152b9f09fe
leandrocgsi <leandrocgsi@gmail.com>
2020-09-19 19:52:36 -0300

    Configuring CORS

---
+++ 14_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
