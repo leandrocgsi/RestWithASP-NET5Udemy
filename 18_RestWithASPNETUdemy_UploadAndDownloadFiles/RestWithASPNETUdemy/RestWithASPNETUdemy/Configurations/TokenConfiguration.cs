dcff7df761260745af8b8ac72f03a841fab89d77
leandrocgsi <leandrocgsi@gmail.com>
2020-09-23 12:27:19 -0300

    Implementing Upload and Download Files

---
+++ 18_RestWithASPNETUdemy_UploadAndDownloadFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Configurations/TokenConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETUdemy.Configurations
+{
+    public class TokenConfiguration
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string Secret { get; set; }
+        public int Minutes { get; set; }
+        public int DaysToExpiry { get; set; }
+    }
+}
---
+++ 18_RestWithASPNETUdemy_UploadAndDownloadFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/Enricher/PersonEnricher.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Hypermedia.Enricher
+{
+    public class PersonEnricher : ContentResponseEnricher<PersonVO>
+    {
+        private readonly object _lock = new object();
+        protected override Task EnrichModel(PersonVO content, IUrlHelper urlHelper)
+        {
+            var path = "api/person/v1";
+            string link = GetLink(content.Id, urlHelper, path);
+
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultGet
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPut
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PATCH,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPatch
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = link,
+                Rel = RelationType.self,
+                Type = "int"
+            });
+            return null;
+        }
+
+        private string GetLink(long id, IUrlHelper urlHelper, string path)
+        {
+            lock (_lock)
+            {
+                var url = new { controller = path, id = id };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).Replace("%2F", "/").ToString();
+            };
+        }
+    }
+}
