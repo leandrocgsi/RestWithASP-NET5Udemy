967efb408bf670c5fb28f49ec082e010e25c1ab6
leandrocgsi <leandrocgsi@gmail.com>
2020-09-23 12:13:24 -0300

    Working with Query Params and Paged Search

--- 17_RestWithASPNETUdemy_QueryParamsAndPagedSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
+++ 17_RestWithASPNETUdemy_QueryParamsAndPagedSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -29,21 +29,22 @@ namespace RestWithASPNETUdemy.Business.Implementations
         public PagedSearchVO<PersonVO> FindWithPagedSearch(
             string name, string sortDirection, int pageSize, int page)
         {
-            var offset = page > 0 ? (page - 1) * pageSize : 0;
             var sort = (!string.IsNullOrWhiteSpace(sortDirection)) && !sortDirection.Equals("desc") ? "asc" : "desc";
-            var size = (pageSize < 1) ? 1 : pageSize;
+            var size = (pageSize < 1) ? 10 : pageSize;
+            var offset = page > 0 ? (page - 1) * size : 0;
 
-            string query = @"select * from Person p where 1 = 1 ";
-            if (!string.IsNullOrWhiteSpace(name)) query = query + $" and p.name like '%{name}%' ";
-            query += $" order by p.firstName {sort} limit {size} offset {offset}";
+            string query = @"select * from person p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(name)) query = query + $" and p.first_name like '%{name}%' ";
+            query += $" order by p.first_name {sort} limit {size} offset {offset}";
             
-            string countQuery = @"select count(*) from Person p where 1 = 1 ";
+            string countQuery = @"select count(*) from person p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(name)) countQuery = countQuery + $" and p.first_name like '%{name}%' ";
 
             var persons = _repository.FindWithPagedSearch(query);
             int totalResults = _repository.GetCount(countQuery);
 
             return new PagedSearchVO<PersonVO> { 
-                CurrentPage = offset,
+                CurrentPage = page,
                 List = _converter.Parse(persons),
                 PageSize = size,
                 SortDirections = sort,
--- 17_RestWithASPNETUdemy_QueryParamsAndPagedSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ 17_RestWithASPNETUdemy_QueryParamsAndPagedSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -31,15 +31,19 @@ namespace RestWithASPNETUdemy.Controllers
 
         // Maps GET requests to https://localhost:{port}/api/person
         // Get no parameters for FindAll -> Search All
-        [HttpGet]
+        [HttpGet("{sortDirection}/{pageSize}/{page}")]
         [ProducesResponseType((200), Type = typeof(List<PersonVO>))]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
         [TypeFilter(typeof(HyperMediaFilter))]
-        public IActionResult Get()
+        public IActionResult Get(
+            [FromQuery] string name,
+            string sortDirection,
+            int pageSize,
+            int page)
         {
-            return Ok(_personBusiness.FindAll());
+            return Ok(_personBusiness.FindWithPagedSearch(name, sortDirection, pageSize, page));
         }
 
         // Maps GET requests to https://localhost:{port}/api/person/{id}
