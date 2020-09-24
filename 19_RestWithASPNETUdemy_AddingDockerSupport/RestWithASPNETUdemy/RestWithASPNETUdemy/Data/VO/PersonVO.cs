ff250adb43f14973e956a995dcba77f409cf7826
leandrocgsi <leandrocgsi@gmail.com>
2020-09-23 21:14:26 -0300

    Adding Docker Support

---
+++ 19_RestWithASPNETUdemy_AddingDockerSupport/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,23 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
---
+++ 19_RestWithASPNETUdemy_AddingDockerSupport/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disable(long id);
+        List<Person> FindByName(string firstName, string secondName);
+    }
+}
