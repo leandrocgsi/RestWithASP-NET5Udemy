0571ab6fce3b7b55caea417631ba46683ba42c9b
Leandro Costa <leandrocgsi@gmail.com>
2020-09-25 21:24:22 -0300

    Continuous Integration with Github Actions and Azure

--- 20_RestWithASPNETUdemy_DeployToAzure/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 20_RestWithASPNETUdemy_DeployToAzure/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -121,7 +121,7 @@ namespace RestWithASPNETUdemy
                 c.SwaggerDoc("v1",
                     new OpenApiInfo
                     {
-                        Title = "REST API's From 0 to Azure with ASP.NET Core 5 and Docker",
+                        Title = "REST API's From 0 to Azure with ASP.NET Core 5 and Docker - Foo Bar",
                         Version = "v1",
                         Description = "API RESTful developed in course 'REST API's From 0 to Azure with ASP.NET Core 5 and Docker'",
                         Contact = new OpenApiContact
