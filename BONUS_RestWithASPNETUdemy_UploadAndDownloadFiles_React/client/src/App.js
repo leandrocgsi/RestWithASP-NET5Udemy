90b589d4f5d6292e3bb63111b9699eef8a777f7b
leandrocgsi <leandrocgsi@gmail.com>
2020-11-05 01:43:48 -0300

    Consuming API with React

--- BONUS_RestWithASPNETUdemy_UploadAndDownloadFiles_React/client/src/App.js
+++ BONUS_RestWithASPNETUdemy_UploadAndDownloadFiles_React/client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Header from './Header'
+import './global.css';
+
+import Login from './pages/Login'
 
 export default function App() {
   return (
-    <Header>
-      Client REST Udemy - Properties
-    </Header>
+    <Login/>
   );
 }
