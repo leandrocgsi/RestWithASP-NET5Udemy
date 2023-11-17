c465a35c53cd1389e1c0a2909c146317ec7bebb6
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 09:27:57 -0300

    Working with four main Verbs

---
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETErudio/RestWithASPNETErudio/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETErudio.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,23 @@
+using RestWithASPNETErudio.Services.Implementations;
+using RestWithASPNETErudio.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
