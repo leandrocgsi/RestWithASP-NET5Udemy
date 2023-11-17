f4ad660fef29aee95bd8e46611256c469408305b
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 13:06:50 -0300

    Adding Swagger Support

--- 13_RestWithASPNETUdemy_Swagger/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
+++ 13_RestWithASPNETUdemy_Swagger/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -10,12 +10,32 @@ using RestWithASPNETErudio.Repository;
 using Microsoft.Net.Http.Headers;
 using RestWithASPNETErudio.Hypermedia.Enricher;
 using RestWithASPNETErudio.Hypermedia.Filters;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
+var appName = "REST API's RESTful from 0 to Azure with ASP.NET Core 8 and Docker";
+var appVersion = "v1";
+var appDescription = $"REST API RESTful developed in course '{appName}'";
 
-// Add services to the container.
-
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc(appVersion,
+        new OpenApiInfo
+        {
+            Title = appName,
+            Version = appVersion,
+            Description = appDescription,
+            Contact = new OpenApiContact
+            {
+                Name = "Leandro Costa",
+                Url = new Uri("https://pub.erudio.com.br/meus-cursos")
+            }
+        });
+});
 
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
@@ -56,6 +76,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{appName} - {appVersion}");});
+
+var option = new RewriteOptions();
+option.AddRedirect("^$", "swagger");
+app.UseRewriter(option);
 
 app.UseAuthorization();
 
