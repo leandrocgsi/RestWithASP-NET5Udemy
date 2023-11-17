0db1b5462679b0abcfa45afbe6a3525a518033fa
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 11:08:13 -0300

    Working with Content Negotiation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETErudio/RestWithASPNETErudio/Data/VO/BookVO.cs
@@ -0,0 +1,16 @@
+﻿namespace RestWithASPNETErudio.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+    }
+
+}
