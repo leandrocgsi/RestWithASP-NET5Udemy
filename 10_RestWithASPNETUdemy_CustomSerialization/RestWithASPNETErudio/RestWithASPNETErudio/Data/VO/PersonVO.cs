0fdcf8ef487c231f25d043dcd41f068d26a035ff
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 11:02:12 -0300

    Custom JSON Serialization

--- 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETErudio/RestWithASPNETErudio/Data/VO/PersonVO.cs
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETErudio/RestWithASPNETErudio/Data/VO/PersonVO.cs
@@ -1,15 +1,22 @@
-﻿namespace RestWithASPNETErudio.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNETErudio.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
 
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
 
+        [JsonIgnore]
         public string Address { get; set; }
 
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
