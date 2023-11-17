f71ee772026b01fd523ec2f86d8bce1ff4d950bc
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 11:00:35 -0300

    Custom JSON Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,67 @@
+using RestWithASPNETErudio.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETErudio.Business.Implementations;
+using RestWithASPNETErudio.Business;
+using MySqlConnector;
+using Serilog;
+using EvolveDb;
+using RestWithASPNETErudio.Repository.Generic;
+using RestWithASPNETErudio.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0,29)))
+);
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+void MigrateDatabase(string? connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
