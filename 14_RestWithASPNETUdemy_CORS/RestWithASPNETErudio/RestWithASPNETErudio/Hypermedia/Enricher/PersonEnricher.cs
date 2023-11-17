49a532c8c8651c7ded7c021261eaaf5f2beab763
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 13:18:48 -0300

    Configuring CORS

---
+++ 14_RestWithASPNETUdemy_CORS/RestWithASPNETErudio/RestWithASPNETErudio/Hypermedia/Enricher/PersonEnricher.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETErudio.Data.VO;
+using RestWithASPNETErudio.Hypermedia.Constants;
+using System.Text;
+
+namespace RestWithASPNETErudio.Hypermedia.Enricher
+{
+    public class PersonEnricher : ContentResponseEnricher<PersonVO>
+    {
+        protected override Task EnrichModel(PersonVO content, IUrlHelper urlHelper)
+        {
+            var path = "api/person";
+            string link = GetLink(content.Id, urlHelper, path);
+
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultGet
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = link,
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPut
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = link,
+                Rel = RelationType.self,
+                Type = "int"
+            });
+            return Task.CompletedTask;
+        }
+
+        private string GetLink(long id, IUrlHelper urlHelper, string path)
+        {
+            lock (this)
+            {
+                var url = new { controller = path, id };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).Replace("%2F", "/").ToString();
+            };
+        }
+    }
+}
