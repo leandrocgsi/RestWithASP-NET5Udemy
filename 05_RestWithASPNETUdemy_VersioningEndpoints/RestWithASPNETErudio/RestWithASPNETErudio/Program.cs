5af2daee959ac20666d13bc30aa5f091e8a38a28
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 09:55:41 -0300

    Versioning Endpoints

---
+++ 05_RestWithASPNETUdemy_VersioningEndpoints/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,31 @@
+using RestWithASPNETErudio.Services.Implementations;
+using RestWithASPNETErudio.Services;
+using RestWithASPNETErudio.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0,29)))
+);
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
