23dcb5778c5a554f861c9ad50f39a8f4c55b13b1
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 10:44:15 -0300

    Working With Generic Repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETErudio/RestWithASPNETErudio/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETErudio.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETErudio.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,71 @@
+using RestWithASPNETErudio.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETErudio.Business.Implementations;
+using RestWithASPNETErudio.Business;
+using RestWithASPNETErudio.Repository.Implementations;
+using RestWithASPNETErudio.Repository;
+using EvolveDb;
+using Serilog;
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0,29)))
+);
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+void MigrateDatabase(string? connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
