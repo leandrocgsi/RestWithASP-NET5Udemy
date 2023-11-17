c97c7399fd4c182518225ade6cbd0a5cc6dabe2f
leandrocgsi <leandrocgsi@gmail.com>
2023-11-17 12:53:33 -0300

    Adding Swagger Support

---
+++ 13_RestWithASPNETUdemy_Swagger/RestWithASPNETErudio/RestWithASPNETErudio/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,8 @@
+﻿namespace RestWithASPNETErudio.Data.Converter.Contract
+{
+    public interface IParser<O, D>
+    {
+        D Parse(O origin);
+        List<D> Parse(List<O> origin);
+    }
+}
---
+++ 13_RestWithASPNETUdemy_Swagger/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,85 @@
+using RestWithASPNETErudio.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETErudio.Business.Implementations;
+using RestWithASPNETErudio.Business;
+using MySqlConnector;
+using Serilog;
+using EvolveDb;
+using RestWithASPNETErudio.Repository.Generic;
+using RestWithASPNETErudio.Repository;
+using Microsoft.Net.Http.Headers;
+using RestWithASPNETErudio.Hypermedia.Enricher;
+using RestWithASPNETErudio.Hypermedia.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0,29)))
+);
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+builder.Services.AddMvc(options =>
+{
+    options.RespectBrowserAcceptHeader = true;
+
+    options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+    options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+})
+.AddXmlSerializerFormatters();
+
+var filterOptions = new HyperMediaFilterOptions();
+filterOptions.ContentResponseEnricherList.Add(new PersonEnricher());
+filterOptions.ContentResponseEnricherList.Add(new BookEnricher());
+
+builder.Services.AddSingleton(filterOptions);
+
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapControllerRoute("DefaultApi", "{controller=values}/v{version=apiVersion}/{id?}");
+
+app.Run();
+
+
+void MigrateDatabase(string? connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
